bf4357ffca7c1b55dcddc2c3d56d12614f83b5c3
aticcmahbub <aticcmahbub@gmail.com>
2024-08-17 13:56:05 +0600

    environment varriables setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,8 +15,8 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://mega_mart_admin:mw8bvSMbpn7Ahm9c@cluster0.ms3r4qf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// const uri = "mongodb+srv://<username>:<password>@cluster0.ms3r4qf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://mega_mart_admin:mw8bvSMbpn7Ahm9c@cluster0.ms3r4qf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ms3r4qf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
